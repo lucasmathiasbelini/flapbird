cbdb1898f8865b2fedc7266f930038acd0f87934
lucas <lucas.belini@escola.pr.gov.br>
2024-10-10 11:52:48 -0300

    joia

--- flapbird/MainPage.xaml.cs
+++ flapbird/MainPage.xaml.cs
@@ -2,11 +2,36 @@
 
 public partial class MainPage : ContentPage
 {
+	const int gravidade = 1;
+	const int tempoentreframes = 25;
+	bool estaMorto = false;
 	
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-
+	void AplicaGravidade(){
+		Bird.TranslationY += gravidade;
+	}
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenha();
+    }
+	async Task Desenha(){
+		while(!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoentreframes);
+		}
+	}
+	void OnGameOverCliked(object s,TappedEventArgs a){
+		FrameGameOver.IsVisible= false;
+		Inicializar();
+		Desenha();
+	}
+	void Inicializar(){
+		Bird.TranslationY=0;
+	}
 }
 
