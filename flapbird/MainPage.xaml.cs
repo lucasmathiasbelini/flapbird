6a3a6a900947972f6d7bb7ba665432116c09d79d
lucas <lucas.belini@escola.pr.gov.br>
2024-10-17 11:50:47 -0300

    joia

--- flapbird/MainPage.xaml.cs
+++ flapbird/MainPage.xaml.cs
@@ -2,18 +2,19 @@
 
 public partial class MainPage : ContentPage
 {
+    int score = 0;
+
     double larguraJanela = 0;
     double alturaJanela = 0;
     int velocidade = 20;
 
     const int gravidade = 1;
     const int tempoentreframes = 25;
-    bool estaMorto = false;
-
     const int forcaPulo = 30;
     const int maxTempoPulando = 3;
 
     bool estaPulando = false;
+    bool estaMorto = true;
     int TempoPulando = 0;
 
     public MainPage()
@@ -26,18 +27,12 @@ public partial class MainPage : ContentPage
         Bird.TranslationY += gravidade;
     }
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-        Inicializar();  
-        Desenha();
-    }
-
     async Task Desenha()
     {
         while (!estaMorto)
         {
-            if (estaPulando)
+            GerenciarCanos();
+            if(estaPulando)
             {
                 AplicaPulo();
             }
@@ -45,17 +40,21 @@ public partial class MainPage : ContentPage
             {
                 AplicaGravidade();
             }
-
+            if (VerificaColisao())
+			{
+				estaMorto = true;
+				GameOverGrid.IsVisible = true;
+				break;
+			}
             await Task.Delay(tempoentreframes);
-            GerenciarCanos();
         }
     }
 
-    void OnGameOverCliked(object s, TappedEventArgs a)
+    void OnGameOverClicked(object s, TappedEventArgs a)
     {
         GameOverGrid.IsVisible = false;
         Inicializar();
-        Desenha();
+        Desenha(); 
     }
 
     void Inicializar()
@@ -78,8 +77,8 @@ public partial class MainPage : ContentPage
 
         if (BottomPipe.TranslationX < -larguraJanela)
         {
-            BottomPipe.TranslationX = 13;
-            TopPipe.TranslationX = 13;
+            BottomPipe.TranslationX = 12;
+            TopPipe.TranslationX = 0;
         }
     }
 
@@ -95,50 +94,37 @@ public partial class MainPage : ContentPage
         }
     }
 
-    void Ui(object s, TappedEventArgs e)
+    void OnGridCliked(object s, TappedEventArgs args)
     {
-       
             estaPulando = true;
         
     }
-bool VerificaColisao()
-{
-	if(!estaMorto)
-	{
-		if(VerificaColisaoTeto() || VerificaColisaoChao())
-		{
-			return true;
-		}
-	}
-	return false;
-}
+    bool VerificaColisao()
+    {
+        if(!estaMorto)
+        {
+            if(VerificaColisaoTeto() || VerificaColisaoChao())
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
 	bool VerificaColisaoTeto(){
-		var minY =- alturaJanela/2;
+		var minY =- alturaJanela / 2;
 		if(Bird.TranslationY <= minY)
-		return true;
+		    return true;
 		else
-		return false;
+		    return false;
 	}
+
 	bool VerificaColisaoChao(){
-	var maxY = alturaJanela / 2 = chao.HeightRequest;
+	var maxY = alturaJanela / 2 - chao.HeightRequest;
 		if(Bird.TranslationY >= maxY)
-		return true;
+		    return true;
 		else
-		return false;
-	}
-	async Task desenha(){
-		while(!estaMorto){
-			AplicaGravidade();
-			GerenciarCanos();
-			if(VerificaColisao())
-			{
-				estaMorto = true;
-				GameOverGrid.IsVisible = true;
-				break;
-			}
-			await Task.Delay(tempoentreframes);
-		}
+		    return false;
 	}
 }
 
